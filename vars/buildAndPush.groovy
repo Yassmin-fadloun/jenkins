18dc0cdba4ed000cb45a738aa75fcc19af6c2cda
Ubuntu <ubuntu@ip-172-31-87-144.ec2.internal>
2025-07-30 17:07:51 +0000

    Fixed shared library implementation

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,12 +1,16 @@
-def buildImage(imageName, imageTag) {
-    sh "docker build -t ${imageName}:${imageTag} ."
-}
+def call(Map args = [:]) {
+    def imageName = args.imageName
+    def imageTag = args.imageTag
 
-def pushImage(imageName, imageTag) {
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh """
-        echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-        docker push ${imageName}:${imageTag}
-        """
+    echo "Building Docker image ${imageName}:${imageTag}"
+    sh "docker build -t ${imageName}:${imageTag} ."
+    
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-creds',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PWD'
+    )]) {
+        sh "echo \$DOCKER_PWD | docker login -u \$DOCKER_USER --password-stdin"
+        sh "docker push ${imageName}:${imageTag}"
     }
 }
